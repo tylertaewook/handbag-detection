9a513f85303440a54d0c2da30019265d8c4fc337
tylertaewook <tk2891@columbia.edu>
2022-02-03 15:08:24 +0900

    changed parameters @run_ms.sh

--- run_ms.sh
+++ run_ms.sh
@@ -1,2 +1,1 @@
-# press any key to progress through different imgs and scales
-python multiscale.py -t ./logos/g2.png -i dataset -s 0.23 -v False
+python multiscale.py -t ./logos -i ./dataset -s 0.235
